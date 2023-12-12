9b87b24b7d87b3b3ea3cd48e174ff0bc499964c5
amin <aminevaaali@gmail.com>
2023-12-12 16:03:21 +0330

    Updates

---
+++ precache-manifest.cf3e56e4c30956df8ce02deba709ed04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86ca03557cb2425ff8f100b279f04cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30e51890806786047a65",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "ec96a827fcd5998ca77e",
+    "url": "./static/js/2.be8a5260.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.be8a5260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e51890806786047a65",
+    "url": "./static/js/main.63b10647.chunk.js"
+  },
+  {
+    "revision": "f749b2b4923a5f0f0345",
+    "url": "./static/js/runtime-main.ef018755.js"
+  }
+]);
