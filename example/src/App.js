77d78b4f1cfd0dd0b079792e7747166923af9764
amin <aminevaaali@gmail.com>
2024-01-23 01:28:47 +0330

    removing ExampleComponent from index.js and removing styles.module.css file

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-import { ExampleComponent, BlueRed } from 'nreact-colors'
 import 'nreact-colors/dist/index.css'
 
 const App = () => {
   return <div>
-    <ExampleComponent text="Create React Library Example 😄" />
     <p style={{color: BlueRed['accent']}}>This is test text</p>
   </div>
 }
