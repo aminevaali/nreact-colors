1466ffe747e52b40e7cfcdc088ee5bedb630a7e8
amin <aminevaaali@gmail.com>
2024-01-23 01:13:18 +0330

    adding OrangeBlack color pallete

--- src/index.js
+++ src/index.js
@@ -15,3 +15,14 @@ export const BlueRed = {
   secondaryText: "#757575",
   dividerColor: "#BDBDBD"
 }
+
+export const OrangeBlack = {
+  primary: "#2196F3",
+  darkPrimary: "#E64A19",
+  lightPrimary: "#FFAB91",
+  text_icon: "#FFFFFF",
+  accent: "#212121",
+  primaryText: "#212121",
+  secondaryText: "#757575",
+  dividerColor: "#BDBDBD"
+}
